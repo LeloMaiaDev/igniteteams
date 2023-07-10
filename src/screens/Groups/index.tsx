65c365afe84a0edb1a295e651a82616a89fac977
“Lelo <lelo@tonavilla.com.br>
2023-07-09 22:00:41 -0300

    Component ListEmpty criado e inserido na aplicação.

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -4,6 +4,7 @@ import { FlatList } from 'react-native';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight';
 import { GroupCard } from '@components/GroupCard';
+import { ListEmpty } from '@components/ListEmpty';
 
 import { Container } from './styles';
 
@@ -24,6 +25,12 @@ const [groups, setGroups] = useState<string[]>([]);
         renderItem={({ item }) => (
           <GroupCard
             title={item}
+          />
+        )}
+        contentContainerStyle={groups.length === 0 && {flex: 1 }}
+        ListEmptyComponent={() => (
+          <ListEmpty
+            message='Que tal cadastrar a primeira turma?'
           />
         )}
       />
