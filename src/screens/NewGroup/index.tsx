9443471fea819715008d2304a5e42dbaa7ee6d64
“Lelo <lelo@tonavilla.com.br>
2023-07-11 21:27:27 -0300

    Componente Input criado e adicionado à screen NewGroup.

--- src/screens/NewGroup/index.tsx
+++ src/screens/NewGroup/index.tsx
@@ -3,8 +3,9 @@ import { Container, Content, Icon } from "./styles";
 import { Header } from "@components/Header";
 import { Highlight } from "@components/Highlight";
 import { Button } from "@components/Button";
+import { Input } from "@components/Input";
 
-export function NewGroup() {
+export function NewGroup(){
     return(
         <Container>
             <Header showBackButton />
@@ -14,8 +15,12 @@ export function NewGroup() {
                     title='Nova turma'
                     subtitle='Crie a turma para adicionar pessoas'
                 />
+                <Input
+                    placeholder='Nome da turma'
+                />
                 <Button
                     title='Criar'
+                    style={{ marginTop: 10 }}
                 />
             </Content>
         </Container>
