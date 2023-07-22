09c670e28b337a7c60eacc1a314e547bd3ff8d05
“Lelo <lelo@tonavilla.com.br>
2023-07-22 09:29:07 -0300

    Mapeamento de Routes excluído de Babel.config e tsconfig. Index criado para Routes e configurado. Header das rotas omitido da aplicação.

--- src/routes/app.routes.tsx
+++ src/routes/app.routes.tsx
@@ -8,7 +8,9 @@ const { Navigator, Screen } = createNativeStackNavigator();
 
 export function AppRoutes(){
     return(
-        <Navigator>
+        <Navigator
+            screenOptions={{ headerShown: false }}
+        >
             <Screen
                 name='groups'
                 component={Groups}
